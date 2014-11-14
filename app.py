5f534295d13e8358884e5c10c8fe775b689bce2d
Kristoffer Dalby <kradalby@kradalby.no>
2014-11-14 14:14:11 +0100

    I am sorry

--- app.py
+++ app.py
@@ -16,18 +16,34 @@ app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html', dict=dict)
+    return render_template('templates/index.html')
 
 @app.route('/about')
 def about():
-    return render_template('templates/about.html', dict=dict)
+    return render_template('templates/about.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('templates/faq.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('templates/contact.html')
+
+@app.route('/sitemap')
+def sitemap():
+    return render_template('templates/site_map.html', dict=dict)
+
 
 @app.route('/car/')
 @app.route('/car/<int:car_id>')
 def car(car_id=None):
-    return "derp %d" % car_id
-    return render_template('templates/car.html', dict=dict)
-    return render_template('templates/cars.html', dict=dict)
+    if car_id:
+        car = Car.query.get(car_id)
+        return render_template('templates/car.html', car=car)
+
+    cars = Car.query.all()
+    return render_template('templates/cars.html', cars=cars)
 
 
 
