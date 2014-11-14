10db3c55710ddf2fca4f81f3c6f6f7b27baad8c4
mythern <myth@overflow.no>
2014-11-14 13:42:31 +0100

    Add sqlite proper path

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % os.path.dirname(os.path.abspath(__file__))
 db = SQLAlchemy(app)
